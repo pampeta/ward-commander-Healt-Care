8ca4a4c857890226eb5eaa4d795558c71b7456dd
pampeta <pedropablobenjamin14@gmail.com>
2026-07-21 19:13:00 -0300

    Sincronizar proyecto con supabase

--- src/Services/supabase.ts
+++ src/Services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://ttjvobecokbzlquwagqz.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0anZvYmVjb2tiemlxdXdhZ3F6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NjEyMDgsImV4cCI6MjEwMDIzNzIwOH0.WyLpM-3Wnm_FGi1SZ-QIruK6uJ4Xk_OUmEs_bB9fAVI';
+const SUPABASE_URL = 'https://xjyfjfbwyuiqbxjfgnhe.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_N9zA8b9NC5Tt_wK5WbbFAA_OslXaX1i';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
