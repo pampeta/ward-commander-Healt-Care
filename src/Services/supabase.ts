f555a59451df1b46667842450be5d271938b0c86
pampeta <pedropablobenjamin14@gmail.com>
2026-07-21 18:35:45 -0300

    Corregir sintaxis en supabase.ts

--- src/Services/supabase.ts
+++ src/Services/supabase.ts
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL = 'https://ttjvobecokbzlquwagqz.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_0eC-5AqGkfYFHwvihwOeQ_n64m-xab';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
