9f2df0d37b4554583dd768dccbff9b1516ef8ac7
pampeta <pedropablobenjamin14@gmail.com>
2026-08-11 21:40:26 -0300

    Actualización de vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,10 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; import tailwindcss from '@tailwindcss/vite'; export default defineConfig({ plugins: [react(), tailwindcss()] }); 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1200, // Aumenta el límite de advertencia a 1200 kB para evitar el aviso
+  },
+});
