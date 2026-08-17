a4a617f3b3c2045d0fba924417aa605dc2cbc49d
pampeta <pedropablobenjamin14@gmail.com>
2026-08-16 21:42:55 -0300

    Resolver problemas quizas

--- src/Services/supabase.ts
+++ src/Services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://xjyfjfbwyuiqbxjfgnhe.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_N9zA8b9NC5Tt_wK5WbbFAA_OslXaX1i';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
